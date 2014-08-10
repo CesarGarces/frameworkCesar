334fbc389cdffd08803c00a3bba531b8248941a9
CesarGarces <cesargarce@gmail.com>
2014-08-10 16:53:21 -0500

    subida

---
+++ application/Config.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+
+define('BASE_URL', 'http://localhost/mvc/');
+define('DEFAULT_CONTROLLER', 'index');
+define('DEFAULT_LAYOUT', 'default');
+
+define('APP_NAME', 'Mi Framework');
+define('APP_SLOGAN', 'mi primer framework php y mvc...');
+define('APP_COMPANY', 'www.dlancedu.com');
+define('SESSION_TIME', 10);
+define('HASH_KEY', '4f6a6d832be79');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '1234');
+define('DB_NAME', 'mvc');
+define('DB_CHAR', 'utf8');
+
+?>
